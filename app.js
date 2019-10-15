a4b6f33bdedbf6a5221474440c256fad081577fd
asaran97 <asaran234@gmail.com>
2019-10-15 18:40:56 +0530

    user signup

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect('mongodb+srv://node-shop:'+process.env.MONGO_ATLAS_PW+'@node-rest-shop-vvbkc.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -30,6 +31,7 @@ app.use((req,res,next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
